985670848660ec3839cca8b2ec735655f4e8fa95
Chris R <thebookwormchris@gmail.com>
2021-10-23 12:02:03 -0700

    Collision and Basic Movement Animation for Player
    The player now collides with objects and has some basic animation using test sprites.

--- BuddyLivesAnotherDay/Player.gd
+++ BuddyLivesAnotherDay/Player.gd
@@ -1,11 +1,22 @@
-extends Area2D
+extends KinematicBody2D
 
+enum MovementState{
+	IDLE,
+	UP,
+	LEFT,
+	DOWN,
+	RIGHT
+}
+
+var oldState = MovementState.IDLE
+var state = MovementState.IDLE
+var velocity := Vector2()
 const MOVE_SPEED := 100
 
-func _ready():
-	pass
+func _ready() -> void:
+	$AnimatedSprite.play("Idle");
 
-func _process(delta):
+func GetVelocity() -> Vector2:
 	var moveDirection := Vector2()
 	if Input.is_action_pressed("worldRight"):
 		moveDirection.x += 1
@@ -15,4 +26,40 @@ func _process(delta):
 		moveDirection.y -= 1
 	if Input.is_action_pressed("worldDown"):
 		moveDirection.y += 1
-	position += moveDirection.normalized() * MOVE_SPEED * delta
+	return moveDirection.normalized() * MOVE_SPEED
+
+func UpdateDirection() -> void:
+	oldState = state
+	state = MovementState.IDLE
+	if Input.is_action_pressed("worldRight"):
+		state = MovementState.RIGHT
+	if Input.is_action_pressed("worldLeft"):
+		state = MovementState.LEFT
+	if Input.is_action_pressed("worldUp"):
+		state = MovementState.UP
+	if Input.is_action_pressed("worldDown"):
+		state = MovementState.DOWN
+
+func UpdateAnimation() -> void:
+	if oldState == state:
+		pass
+	var animation := "Idle"
+	match state:
+		MovementState.IDLE:
+			animation = "Idle"
+		MovementState.UP:
+			animation = "WalkUp"
+		MovementState.DOWN:
+			animation = "WalkDown"
+		MovementState.LEFT:
+			animation = "WalkLeft"
+		MovementState.RIGHT:
+			animation = "WalkRight"
+	$AnimatedSprite.play(animation)
+
+func _process(delta: float) -> void:
+	UpdateDirection()
+	UpdateAnimation()
+
+func _physics_process(delta: float) -> void:
+	velocity = move_and_slide(GetVelocity())
