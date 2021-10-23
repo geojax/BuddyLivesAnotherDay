10ea3b7d6c3808e3954cc035578a399fa6086550
geojax <50026101+geojax@users.noreply.github.com>
2021-10-23 12:36:54 -0700

    ArmWrestle Script tidying

--- BuddyLivesAnotherDay/Scripts/ArmWrestle.gd
+++ BuddyLivesAnotherDay/Scripts/ArmWrestle.gd
@@ -15,28 +15,34 @@ onready var exposeDelayTimer = get_node("ExposeDelayTimer")
 onready var exposeTimer = get_node("ExposeTimer")
 onready var stunTimer = get_node("StunTimer")
 onready var fightProgressBar = get_node("FightProgressBar")
-func _ready():
-	fightProgressBar.value = 50
-	exposeDelayTimer.start(floor(rand_range(MIN_EXPOSE_DELAY_TIME, MAX_EXPOSE_DELAY_TIME)))
-	pass
 
-func _process(delta):
+func UpdateFight() -> void:
 	match state:
 		FightState.NORMAL:
 			fightProgressBar.value -= .5
 			if Input.is_action_just_pressed("armWrestlePush"):
 				fightProgressBar.value += 5
+			pass
+			
 		FightState.EXPOSED:
 			if Input.is_action_just_pressed("armWrestleStun"):
 				exposeTimer.stop()
 				state = FightState.STUNNED
 				print("Stunned!")
 				stunTimer.start(1)
+			pass
+			
 		FightState.STUNNED:
 			if Input.is_action_just_pressed("armWrestlePush"):
 				fightProgressBar.value += 3
 
-	pass
+func _ready():
+	fightProgressBar.value = 50
+	exposeDelayTimer.start(floor(rand_range(MIN_EXPOSE_DELAY_TIME, MAX_EXPOSE_DELAY_TIME)))
+
+func _process(delta):
+	UpdateFight()
+
 
 func _on_ExposeDelayTimer_timeout():
 	print("PRESS RIGHT!")
