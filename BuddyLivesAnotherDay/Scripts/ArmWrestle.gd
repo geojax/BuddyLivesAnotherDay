8ef90ede2f06e24981f103013c22de465d644f3d
geojax <50026101+geojax@users.noreply.github.com>
2021-10-24 16:34:19 -0700

    Starting to implement stamina, no more constant spamming
    Stamina bar is there, doesn't do anything currently though. Instead of constant spamming, only spam when enemy is stunned.

--- BuddyLivesAnotherDay/Scripts/ArmWrestle.gd
+++ BuddyLivesAnotherDay/Scripts/ArmWrestle.gd
@@ -7,56 +7,76 @@ enum FightState {
 }
 var state = FightState.NORMAL
 
-const STUNTIME = 1
+const STUN_TIME = 1
 const MIN_EXPOSE_DELAY_TIME = 5
 const MAX_EXPOSE_DELAY_TIME = 15
 
-onready var exposeDelayTimer = get_node("ExposeDelayTimer")
-onready var exposeTimer = get_node("ExposeTimer")
-onready var stunTimer = get_node("StunTimer")
-onready var fightProgressBar = get_node("FightProgressBar")
+# how fast progress/stamina increase or decrease.
+var fightProgressDecay = .4
+var pushEffectiveness = .4
+var pushStaminaCost = .4
+var staminaRegen = 2
 
-func UpdateFight() -> void:
-	match state:
-		FightState.NORMAL:
-			fightProgressBar.value -= .5
-			if Input.is_action_just_pressed("armWrestlePush"):
-				fightProgressBar.value += 5
-			pass
-			
-		FightState.EXPOSED:
-			if Input.is_action_just_pressed("armWrestleStun"):
-				exposeTimer.stop()
-				state = FightState.STUNNED
-				print("Stunned!")
-				stunTimer.start(1)
-			pass
-			
-		FightState.STUNNED:
-			if Input.is_action_just_pressed("armWrestlePush"):
-				fightProgressBar.value += 3
+var fightProgress = 50
+var stamina := 100
 
+onready var exposeDelayTimer := get_node("ExposeDelayTimer")
+onready var exposeTimer := get_node("ExposeTimer")
+onready var stunTimer := get_node("StunTimer")
+onready var fightProgressBar := get_node("FightProgressBar")
+onready var staminaProgressBar := get_node("StaminaProgressBar")
+	
+func UpdateProgressBars() -> void:
+	fightProgress = clamp(fightProgress, 0, 100)
+	fightProgressBar.value = fightProgress
+	
+	stamina = clamp(stamina, 0, 100)
+	staminaProgressBar.value = stamina
+	
+func UpdateFightNormal() -> void:
+	fightProgress -= fightProgressDecay
+	if Input.is_action_pressed("armWrestlePush"):
+		fightProgress += pushEffectiveness
+		stamina -= pushStaminaCost
+	else:
+		stamina += staminaRegen
+	
+func UpdateFightExposed() -> void:
+	if Input.is_action_just_pressed("armWrestleStun"):
+		exposeTimer.stop()
+		state = FightState.STUNNED
+		print("Stunned!")
+		stunTimer.start(STUN_TIME)
+		
+func UpdateFightStunned() -> void:
+	if Input.is_action_just_pressed("armWrestlePush"):
+		fightProgress += 3
+	
 func _ready():
 	fightProgressBar.value = 50
 	exposeDelayTimer.start(floor(rand_range(MIN_EXPOSE_DELAY_TIME, MAX_EXPOSE_DELAY_TIME)))
 
 func _process(delta):
-	UpdateFight()
-
-
+	match state:
+		FightState.NORMAL:
+			UpdateFightNormal()
+		FightState.EXPOSED:
+			UpdateFightExposed()
+		FightState.STUNNED:
+			UpdateFightStunned()
+	UpdateProgressBars()
+		
 func _on_ExposeDelayTimer_timeout():
 	print("PRESS RIGHT!")
 	state = FightState.EXPOSED
 	exposeTimer.start(1)
 	pass # Replace with function body.
 
-
 func _on_ExposeTimer_timeout():
 	print("Expose timer end!")
 	state = FightState.NORMAL
 	exposeDelayTimer.start(randi() % 10 + 10)
 	pass # Replace with function body.
-
 
 func _on_StunTimer_timeout():
 	state = FightState.NORMAL
