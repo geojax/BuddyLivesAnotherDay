ebbb42442df7606862f30b995002af659ed53f51
Chris R <thebookwormchris@gmail.com>
2021-10-24 18:38:43 -0700

    Some camera stuff
    now it follows a camera, sometimes

--- BuddyLivesAnotherDay/Scripts/ScreenEffects.gd
+++ BuddyLivesAnotherDay/Scripts/ScreenEffects.gd
@@ -1,5 +1,6 @@
-extends Control
+extends Container
 
+export var cameraActive := false
 export var useEnterExit := false
 export var enterExitFade := 0.001
 export var noiseScale := 0.2
@@ -17,7 +18,9 @@ func UpdateFocus(visibleRect: Rect2) -> void:
 		return
 	var viewportWidth = visibleRect.size.x
 	var viewportHeight = visibleRect.size.y
-	var viewportPos = viewport.get_visible_rect().position
+	var viewportPos
+	if cameraActive: viewportPos = Vector2(player.position.x - visibleRect.size.x/2, player.position.y - visibleRect.size.y/2)
+	else: viewportPos = visibleRect.position
 	var focus = Vector3((player.position.x - viewportPos.x)/viewportWidth, (player.position.y - viewportPos.y)/viewportHeight, 1)
 	$EnterExitEffect.get_material().set_shader_param("focusPoint", focus)
 
@@ -46,11 +49,13 @@ func _ready() -> void:
 	if useEnterExit == true:
 		$EnterExitEffect.get_material().set_shader_param("blurRadius", enterExitFade)
 		PlayEnter()
-		
+
 func _process(delta: float) -> void:
 	var visibleRect = get_viewport().get_visible_rect()
 	var screenRatio = visibleRect.size.x / visibleRect.size.y
 	$VignetteEffect.get_material().set_shader_param("screenRatio", screenRatio)
+	if cameraActive:
+		rect_position = Vector2(player.position.x - visibleRect.size.x/2, player.position.y - visibleRect.size.y/2)
 	if useEnterExit: 
 		UpdateFocus(visibleRect)
 		$EnterExitEffect.get_material().set_shader_param("screenRatio", screenRatio)
