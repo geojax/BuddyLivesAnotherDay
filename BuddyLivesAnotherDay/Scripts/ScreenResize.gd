f98fb31cb1d967593c1769598c2dca4df98ee865
Chris R <thebookwormchris@gmail.com>
2022-01-08 21:16:35 -0800

    Fixed Sub-One
    Fixed the transition zone for and the room Sub-One.

--- BuddyLivesAnotherDay/Scripts/ScreenResize.gd
+++ BuddyLivesAnotherDay/Scripts/ScreenResize.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	if !OS.has_feature("web"):
+	if !OS.has_feature("web") and !OS.has_feature("mobile"):
 		# OS.window_fullscreen = true
 		var size = OS.get_screen_size()
 		var padding = Vector2(size.x / 8, size.x / 8)
@@ -9,6 +9,6 @@ func _ready():
 		OS.center_window()
 
 func _process(_delta):
-	if !OS.has_feature("web"):
+	if !OS.has_feature("web") and !OS.has_feature("mobile"):
 		if Input.is_action_just_pressed("toggleFullscreen"):
 			OS.window_fullscreen = !OS.window_fullscreen
